3b71f1bd910e9e1ab778afe239aeafea219aa4e6
MAREEDU BHARAT VIKAS <144128033+BharatVikas@users.noreply.github.com>
2025-10-03 23:10:54 +0530

    Change displayed name from 'Bharat Vikas' to 'Vicky'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="name-container">
-        <h1 className="name-display">Bharat Vikas</h1>
+        <h1 className="name-display">Vicky</h1>
         <div className="name-subtitle">Welcome to my React App</div>
       </div>
     </div>
