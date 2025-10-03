088b8885e6c369d1cb06af5c8122a647105a2415
Vicky <bharatvikasvikky@gmail.com>
2025-10-03 21:38:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="name-container">
+        <h1 className="name-display">Bharat Vikas</h1>
+        <div className="name-subtitle">Welcome to my React App</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
